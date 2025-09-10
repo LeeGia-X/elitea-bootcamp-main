be52819394342279220eee546c946f60991e8c54
LeeGia-X <iryna.kemko@gmail.com>
2025-09-10 17:36:55 +0300

    Update CucumberTestRunner.java

--- module-taf-kwd/src/test/java/com/staf/kwd/runner/CucumberTestRunner.java
+++ module-taf-kwd/src/test/java/com/staf/kwd/runner/CucumberTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = { "classpath:features/test-1.feature" },
+        features = { "classpath:features/EditExistingPrompt.feature" },
         glue = {"com.staf.kwd.library.steps" })
 public class CucumberTestRunner extends AbstractTestNGCucumberTests{
 }
